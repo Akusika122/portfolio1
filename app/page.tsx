df703310c0437306a2d84c146ad5df99976be877
Akusika122 <akusikafortune77@gmail.com>
2025-11-07 14:50:11 +0000

    made changes in home

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ function Home() {
       <div className='container mx-auto h-full'>
         <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
           <div className='text-center xl:text-left order-2 xl:order-none xl:items-center'>
-            <span className='text-xl'>Frontend Developer</span>
-            <h1 className='h1 my-6'>Hello I'm <span className='text-accent'>Isaac</span></h1>
+            <span className='text-xl'>UI/UX Designer</span>
+            <h1 className='h1 my-6'>Hello I'm <span className='text-accent'>Fortune</span></h1>
 
             <p className='max-w-[500px] mb-9 text-white/80'>I excel at crafting digital experiences and I am proficient in various languages and technologies</p>
 
@@ -20,9 +20,9 @@ function Home() {
           </div>
 
           {/* {photo} */}
-          <div className='order-1 xl:order-none mb-8 xl:mb-0'>
+          {/* <div className='order-1 xl:order-none mb-8 xl:mb-0'>
             <Photo />
-          </div>
+          </div> */}
 
         </div>
       </div>
